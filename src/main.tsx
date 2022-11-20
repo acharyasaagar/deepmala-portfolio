4206612a82d5228a2410052a5c710c0651d3754f
Sagar <acharyasaagar@gmail.com>
2022-11-20 12:58:59 +0200

    Adds mui and sets up with theme provider and css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import CssBaseline from '@mui/material/CssBaseline'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
