ed50a24d421a9e370731e893f20521a6901139fb
Sagar <acharyasaagar@gmail.com>
2022-11-24 20:09:52 +0200

    Adds projects route

---
+++ src/routes/projects/index.tsx
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material'
+import ProjectCard from '../../components/ProjectCard'
+import Topbar from '../../components/Topbar'
+import Fade from 'react-reveal/Fade'
+
+const CardsContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+
+const Projects = () => {
+  return (
+    <div>
+      <Topbar />
+      <CardsContainer>
+        <Fade bottom>
+          <ProjectCard title="Grocery shopping app" imgURL="/photo.jpg" />
+        </Fade>
+        <Fade bottom>
+          <ProjectCard title="Customize sweatshirt" imgURL="/photo.jpg" />
+        </Fade>
+        <Fade bottom>
+          <ProjectCard title="Daily expense tracker app" imgURL="/photo.jpg" />
+        </Fade>
+      </CardsContainer>
+    </div>
+  )
+}
+
+export default Projects
