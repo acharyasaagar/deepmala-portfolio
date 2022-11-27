4abd95576bf4f2872134ab091974e7698e780b39
Sagar <acharyasaagar@gmail.com>
2022-11-27 22:20:35 +0200

    Adds rewriteSlashToIndexHTML plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  } as const
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  appType: 'mpa',
+  plugins: [react(), rewriteSlashToIndexHtml()],
 })
